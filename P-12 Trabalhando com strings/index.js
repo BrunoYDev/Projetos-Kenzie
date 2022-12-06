8ccf2e5f72162dbbb05dddaa6e11aa3401543f1c
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2022-12-06 10:43:56 -0300

    mais exercicios

---
+++ P-12 Trabalhando com strings/index.js
@@ -0,0 +1,18 @@
+
+// Exercicio 1
+// let meuNome = 'Bruno';
+// let comprimentoMeuNome = meuNome.length;
+// console.log(`O meu nome possui ${comprimentoMeuNome} caracteres de comprimento.`);
+
+//Exercicio 2
+// let preco = 15;
+// let precoFormatado = "R$"+preco+",00";
+// console.log(`O valor ${preco} formatado ficaria assim ${precoFormatado}`);
+
+// Exercicio 3
+// let cumprimento = 'Bom dia';
+// let nome = 'Bruno';
+// let sobrenome = "Garcia";
+// let mensagemCumprimento = (`${cumprimento},${nome} ${sobrenome}`);
+
+// console.log(mensagemCumprimento);
