38829c87d840be86bb1466d5e5cc97cd991db663
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2022-12-07 13:25:02 -0300

    ez desafio

---
+++ Atividades/Ac-11 Desafio portaria/script.js
@@ -0,0 +1,16 @@
+let nome = prompt("Digite seu nome:");
+let idade = parseInt(prompt("Digite sua idade:"));
+
+
+if(idade >= 18){
+    let estaAcompanhado = prompt("Digite \"S\" caso esteja acompanhado ou \"N\" caso não esteja");
+    if(estaAcompanhado == "S" || "s"){
+        estaAcompanhado = true;
+        alert(`Desconto concedido e entrada permitida para ${nome} e seu acompanhante!`);
+    }else if(estaAcompanhado == "N" || "n"){
+        estaAcompanhado = false;
+        alert(`Entrada permitida para ${nome}, Valor integral!`);
+    }
+}else{
+    alert(`Entrada negada para ${nome}, idade não permitida!`);
+}
