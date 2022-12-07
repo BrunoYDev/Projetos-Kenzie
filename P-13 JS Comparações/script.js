c353a6644ac742f0e86c65104456a915e1f44209
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2022-12-07 11:33:39 -0300

    Novo projetinho

---
+++ P-13 JS Comparações/script.js
@@ -0,0 +1,20 @@
+// Exercicio 1
+// let valor1 = 10;
+// let valor2 = "10";
+
+// let comparacaoDeValores = valor1 === valor2;
+// console.log(comparacaoDeValores);
+
+// Exercicio 2
+// let user1Height = 1.80;
+// let user2Height = 1.75;
+
+// let comparacaoDeAlturas = user1Height > user2Height;
+// console.log(comparacaoDeAlturas);
+
+// Exercicio 3
+let valor1 = 10;
+let valor2 = "10";
+let comparacaoDeValores = valor1 !== valor2;
+
+console.log(comparacaoDeValores);
