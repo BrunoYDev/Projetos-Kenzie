ff8a987b31a6bfd9b38ac0516463f7e49824d7ff
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2022-12-07 12:41:52 -0300

    Atividade nova

---
+++ P-14 Idade com JS/script.js
@@ -0,0 +1,41 @@
+// Exerccio 1
+// let idade = 24;
+// let avaliarIdade = idade > 18 && idade < 25;
+
+// if(avaliarIdade == true){
+//     console.log("Idade maior que 18 e menor que 25!");
+// }else{
+//     console.log("Idade não se encaixa nos valores maiores que 18 e menores que 25!");
+// }
+
+// Exercicio 2
+// let carroNome = 'Chevrolet';
+// let carroAno = 2019;
+// let filtrarCarro = carroNome=="Chevrolet" && carroAno==2019;
+
+// if(filtrarCarro == true){
+//     console.log("Carro encontrado");
+// }else{
+//     console.log("Carro não encontrado");
+// }
+
+//Exercicio 3
+// let botaoMenu = false;
+// let avaliarClick = botaoMenu != false;
+
+// if(avaliarClick == false){
+//     console.log("Abrir menu");
+//     // se clicar em abrir menu mudar avaliarClick para true
+// }else{
+//     console.log("Fechar menu");
+//     // se clicar em fechar menu mudar avaliarClick para false
+// }
+
+//Exercicio 3 clean
+// let botaoMenu = false;
+
+// if(botaoMenu == false){
+//     console.log("Abrir menu");
+// }else{
+//     console.log("Fechar menu");
+// }
