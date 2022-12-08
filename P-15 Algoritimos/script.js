0b17a7661e44c4d5658a64ca872b01254a3b0ee7
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2022-12-08 13:12:47 -0300

    Ativididades novas

---
+++ P-15 Algoritimos/script.js
@@ -0,0 +1,82 @@
+// Exercicio Exemplo
+// let a = parseInt(prompt("Insira um valor para a"))
+// let b = parseInt(prompt("Insira um valor para b"))
+// let aux = 0
+// aux = a 
+// a = b
+// b = aux
+// alert(`Os valores originais de a e b: ${b}, ${a}. Após a troca são: ${a}, ${b}`)
+
+// Exercicio 1
+// let number = parseInt(prompt("Digite um número"));
+// alert(number);
+
+// Exercicio 2
+// let n1 = parseInt(prompt("Coloque o primeiro número: "));
+// let n2 = parseInt(prompt("Coloque o segundo número: "));
+// let n3 = parseInt(prompt("Coloque o terceiro número: "));
+
+// let soma = n1+n2+n3;
+// alert(`O resultado da soma é: ${soma}`);
+
+// Exercicio 3
+// let primeiroSe = parseFloat(prompt("Digite a nota do primeiro semestre: "));
+// let segundoSe = parseFloat(prompt("Digite a nota do segundo semestre: "));
+
+// let media = (primeiroSe+segundoSe)/2;
+
+// alert(`A média anual desse aluno é: ${media}`);
+
+// Exercicio 4
+// let number = parseInt(prompt("Digite um número: "));
+// let numberRes = number%2;
+
+// alert(`O resto da divisão desse número por 2 é: ${numberRes}`);
+
+// Exercicio 5
+// let number = parseInt(prompt("Digite um número: "));
+// let quadrado = number*number;
+
+// alert(`O quadrado do número ${number} é: ${quadrado}`);
+
+// Exercicio 6
+// let n1 = parseFloat(prompt("Numero 1: "));
+// let n2 = parseFloatprompt("Numero 2: "));
+// let n3 = parseFloat(prompt("Numero 3: "));
+// let n4 = parseFloat(prompt("Numero 4: "));
+// let ponderamento = 1+2+3+4;
+// let ponderada = (n1*1+n2*2+n3*3+n4*4)/ponderamento;
+
+// alert(`A média ponderada dos números digitados é: ${ponderada.toFixed(2)}`);
+
+// Exercicio 7
+// let mat = parseFloat(prompt("Nota de Mátematica: "));
+// let port = parseFloat(prompt("Nota de Português: "));
+// let conGe = parseFloat(prompt("Nota de Conhecimentos Gerais: "));
+// let info = parseFloat(prompt("Nota de Informática: "));
+// let pesoProva = 5;
+
+// let ponderamento = ((mat*2)+port+conGe+info)/pesoProva;
+// alert(`A média ponderada das notas é: ${ponderamento}`);
+
+// Exercicio 8
+// let dividendo = parseInt(prompt("Digite o número que vai ser dividido: "));
+// let divisor = parseInt(prompt("Digite o número divisor: "));
+// let div = dividendo/divisor;
+// let resto = dividendo%divisor;
+
+// alert(`A divisão de ${dividendo} por ${divisor} é: ${div.toFixed(0)} e o resto é ${resto}`);
+
+// Exercicio 9
+// let saldo = parseInt(prompt("Digite um número: "));
+// let reajuste = saldo*1.01;
+// alert(`O reajuste do saldo: ${saldo} é: ${reajuste}`);
+
+// Exercicio 10
+// let d1 = parseInt(prompt("Digite a primeira Diagonal: "));
+// let d2 = parseInt(prompt("Digite a segunda Diagonal: "));
+
+// let multi = d1*d2;
+// let div = multi/2;
+
+// alert(`A área do losango digitado é ${div.toFixed(0)}cm²`);
