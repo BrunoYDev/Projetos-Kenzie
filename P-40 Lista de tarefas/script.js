d81064e340544b05c10b9ead0f25d9ac0a748771
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2023-01-12 17:25:31 -0300

    CARALHO MEU CU PORRAAAAAA TERMINEIIIII CACETEEEE

--- P-40 Lista de tarefas/script.js
+++ P-40 Lista de tarefas/script.js
@@ -1,8 +1,8 @@
 let tasks = [
-    {
-      titulo: "Insira tarefas :)",
-      tipo: "Urgente"
-    }
+    // {
+    //   titulo: "Insira tarefas :)",
+    //   tipo: "Urgente"
+    // },
     // {
     //   titulo: "Limpar o quarto",
     //   tipo: "urgente"
@@ -18,7 +18,7 @@ let tasks = [
     // {
     //   titulo: "Beber água",
     //   tipo: "prioritário"
-    // }
+    // } 
   ];
 
   let lista = document.querySelector('.tasks');
@@ -26,22 +26,28 @@ let tasks = [
   let prio_priori = [];
   let prio_normal = [];
 
+  let newArr2 = [];
+
   function separaItems(list){
     for(let i=0; i<list.length;i++){
         if(list[i].tipo.toLowerCase() == 'urgente'){
+            newArr2.push(list[i].titulo);
             prio_urgente.push(list[i].titulo);
         }else if(list[i].tipo.toLowerCase() == 'prioritário'){
+          newArr2.push(list[i].titulo);
             prio_priori.push(list[i].titulo);
         }else if(list[i].tipo.toLowerCase() == 'normal'){
+            newArr2.push(list[i].titulo);
             prio_normal.push(list[i].titulo);
         }
     }
   }
   separaItems(tasks);
-
+  let cont = 0;
   function renderizarTela(list){
 
     for(let i=0; i<list.length; i++){
+      cont++;
         let listItem = document.createElement('li');
         let listItemParag = document.createElement('p');
         listItem.classList.add('listItem');
@@ -52,12 +58,19 @@ let tasks = [
         deleteButton.setAttribute('class','redBtn');
         deleteButton.innerText = 'Delete';
         deleteButton.addEventListener('click',function(e){
-          list.splice(i,1);
-          listItem.remove('');
+          if(list.length != i){
+            for(let i=0; i<list.length; i++){
+            listItem.remove('');
+            newArr2.splice(i,1);
+            list.splice(i,1);
+            }
+          }
+            listItem.remove('');
+            newArr2.splice(i,1);
+            list.splice(i,1);
         });
         listItem.append(listItemParag,deleteButton);
         lista.appendChild(listItem);
-        
     }
   }
 
@@ -77,21 +90,24 @@ let tasks = [
       titulo: inputValue,
       tipo: inputPrio
     };
-    
     if(inputValue != '' && inputPrio != 'vazio'){
       if(inputPrio == 'urgente'){
+        newArr2.push(newItem.titulo);
         prio_urgente.push(newItem.titulo);
         lista.innerHTML = '';
         renderizarTela(prio_urgente);
         renderizarTela(prio_priori);
         renderizarTela(prio_normal);
       }else if(inputPrio = 'prioritario'){
+        newArr2.push(newItem.titulo);
         prio_priori.push(newItem.titulo);
         lista.innerHTML = '';
         renderizarTela(prio_urgente);
         renderizarTela(prio_priori);
         renderizarTela(prio_normal);
+        console.log(prio_urgente,prio_priori,prio_normal)
       }else if(inputPrio == 'normal'){
+        newArr2.push(newItem.titulo);
         prio_normal.push(newItem.titulo);
         lista.innerHTML = '';
         renderizarTela(prio_urgente);
@@ -105,3 +121,23 @@ let tasks = [
     itemMaker(inputText,selectPrio);
   });
   
+  let search = document.querySelector('.search');
+  let searchBtn = document.querySelector('.searchBtn');
+
+  searchBtn.addEventListener('click', function(e){
+    searchInput(search.value,newArr2);
+  });
+
+  function searchInput(phrase,arr){
+    let normalize = phrase.toLowerCase();
+    let newArr = [];
+    for(let i=0;i<arr.length;i++){
+      if(arr[i].toLowerCase() == normalize){
+        newArr.push(arr[i])
+        lista.innerHTML = '';
+        console.log(newArr)
+      }
+    }
+    renderizarTela(newArr);
+  }
+
